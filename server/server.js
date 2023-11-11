87547b52c417e44ac2697e8cc667c590eacfff36
jsanchezroa@umass.edu <44318828+StuffGames@users.noreply.github.com>
2023-11-11 14:58:52 -0500

    Fixed server.js, added start to package.json

--- server/server.js
+++ server/server.js
@@ -1,16 +1,22 @@
 // import express and morgan logger
 const express = require('express');
 const logger = require('morgan');
+const path = require('path');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;;
 
 app.use(logger('dev'));
 
 // use extra middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use('/', express.static(path.join(__dirname, '../client/html/landing.html')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/html/landing.html'));
+});
 
 app.listen(port, () => {
-    console.log('Listening on port 3000!!! pic-up');
+    console.log(`Listening on pic-up port ${port}!!!`);
 });
