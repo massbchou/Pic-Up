592c8fd9ac59b36c02ff11f5c67e837004e20195
Nate Courchesne <ncourchesne@umass.edu>
2023-11-11 12:37:26 -0500

    Reset Vanilla

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# Pic-Up
+# Pic-Up!
+
+#### **Authors: Nate Courchesne, Ben Chou, Jacob Grassi, Ryan Nguyen, Juan Sanchez Roa, and moral support from Enoch Fu**
+#### *Logo by Tyler Millien*
+
+Take your trash collection to the next level with this --- website!  
+Just take a picture of the trash you collected and we will reward you points your good deed.
---
+++ server/server.js
@@ -0,0 +1,16 @@
+// import express and morgan logger
+const express = require('express');
+const logger = require('morgan');
+
+const app = express();
+const port = 3000;
+
+app.use(logger('dev'));
+
+// use extra middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.listen(port, () => {
+    console.log('Listening on port 3000!!! pic-up');
+});
