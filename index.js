5a01b30727a4871d6466de99034d16f83acf87d1
James Grassi <20jgrassi@gmail.com>
2023-11-11 17:12:34 -0500

    minor update
    added logo detection

--- index.js
+++ index.js
@@ -6,20 +6,23 @@ async function analyse_image(trash) {
     const client = new vision.ImageAnnotatorClient();
   
     // Performs label detection on the image file
-    const [result] = await client.labelDetection(trash);
-    const labels = result.labelAnnotations;
+    const [label_result] = await client.labelDetection(trash);
+    const labels = label_result.labelAnnotations;
+
+    const [logo_result] = await client.logoDetection(trash);
+    const logos = logo_result.logoAnnotations;
 
     label_words = [];
+    logo_words = [];
 
     labels.forEach(label => label_words.push(label.description));
+    logos.forEach(logo => logo_words.push(logo.description));
     
-    console.log('Labels:');
-    labels.forEach(label => console.log(label.description));
+    //console.log('Labels:');
+    //labels.forEach(label => console.log(label.description));
 
-    return new Promise((resolve, reject) => {
-          resolve(label_words);
-      });
+    return [label_words, logo_words];
 
   }
 
-console.log(analyse_image('./images/doritos.jpg'));
+analyse_image('./images/doritos.jpg').then((data) => console.log(data));
