8921ca6fc42e8b5d1d9b6bd0eb6c08c1482ff9d8
James Grassi <20jgrassi@gmail.com>
2023-11-11 18:02:22 -0500

    commenting code
    added some comments

--- index.js
+++ index.js
@@ -9,17 +9,22 @@ async function analyse_image(trash) {
     const [label_result] = await client.labelDetection(trash);
     const labels = label_result.labelAnnotations;
 
+    // Performs logo detection on the image file
     const [logo_result] = await client.logoDetection(trash);
     const logos = logo_result.logoAnnotations;
 
+    //these will be returned
     label_data = [];
     logo_data = [];
 
+    //format the data into arrays to return
     labels.forEach(label => label_data.push(label.description));
     logos.forEach(logo => logo_data.push(logo.description));
 
+    //2d array
     return [label_data, logo_data];
 
   }
 
+  //function call
 analyse_image('./images/doritos.jpg').then((data) => data.forEach(datum => console.log(datum)));
