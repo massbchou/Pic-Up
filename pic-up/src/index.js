7cf58de45b9b60e8184cfc2ad595241feaa48cf2
Nate Courchesne <ncourchesne@umass.edu>
2023-11-11 10:48:35 -0500

    First Push

---
+++ pic-up/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './CSS/App.css';
+import App from './JS/App';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <App />
+);
+
+
